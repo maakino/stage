4d37d01975162689fdefa4b3ce25b007673cf4b1
Maakyno <teddygoss1@hotmail.com>
2022-03-24 08:11:53 +0100

    intégration page activité

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+function isMobileDevice() {
+    if (navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        document.querySelectorAll('.bungalowImg').forEach(function (_conteneur) {
+            _conteneur.addEventListener('click', () => {
+                let hover = _conteneur.querySelector('.bungaon');
+                hover.classList.toggle('bungalow__hover');
+            })
+        })
+    }
+    else {
+        return false;
+    }
+}
+
 let burger = document.getElementById('burger');
 let nav = document.getElementById('nav');
 let link = document.querySelectorAll('.header__navbar-list');
@@ -10,4 +31,10 @@ burger.addEventListener('click', toogleclick);
 link.forEach(function (link) {
     link.addEventListener('click', toogleclick);
 })
+
+
+
+
+
+
 
